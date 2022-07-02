5b89e796e520f8e073da7e62df90bf328d13e51e
Spooked-Potato <arturjddias@hotmail.com>
2022-07-02 19:50:38 +0100

    update v10 - added and correct css

--- src/components/ContactUSComponent.js
+++ src/components/ContactUSComponent.js
@@ -9,60 +9,72 @@ export default function ContactUSComponent() {
     return (
         <>
             <NavComponent />
-            <section className="form-section">
-                <form>
-                    <span> Contact Me</span>
-                    <br />
-
-                    <div className="details-contact">
-                        <label className="label-contact">Name</label>
-                        <input
-                            className="input-contact"
-                            type="text"
-                            placeholder="Type in your name"
-                            name="name"
-                            required
-                        />
+            <section className="form_section">
+                <div className="form_section_container">
+                    <div className="form_section_container_dividor">
+                        <div className="about_me_information">
+                            <h2>test</h2>
+                            <p>test</p>
+                            <h2>test</h2>
+                            <p>test</p>
+                        </div>
                     </div>
+                    <div className="form_section_container_dividor">
+                        <form>
+                            <div className="details_contact">
+                                <label className="label_contact">Name</label>
+                                <input
+                                    className="input_contact"
+                                    type="text"
+                                    placeholder="Type in your name"
+                                    name="name"
+                                    required
+                                />
+                            </div>
 
-                    <div className="details-contact">
-                        <label className="label-contact">Email</label>
-                        <input
-                            className="input-contact"
-                            type="email"
-                            placeholder="Type in your email"
-                            name="email"
-                            required
-                        />
+                            <div className="details_contact">
+                                <label className="label_contact">Email</label>
+                                <input
+                                    className="input_contact"
+                                    type="email"
+                                    placeholder="Type in your email"
+                                    name="email"
+                                    required
+                                />
+                            </div>
+
+                            <div className="details_contact">
+                                <label className="label_contact">
+                                    What are you looking for?
+                                </label>
+                                <textarea
+                                    className="textarea_contact"
+                                    placeholder="Leave here what you are looking for?"
+                                    name="comment"
+                                    required
+                                ></textarea>
+                            </div>
+
+                            <button
+                                type="button"
+                                onClick={() => setAberto(true)}
+                            >
+                                SEND
+                            </button>
+                        </form>
                     </div>
-
-                    <div className="details-contact">
-                        <label className="label-contact">
-                            What are you looking for?
-                        </label>
-                        <textarea
-                            className="textarea-contact"
-                            placeholder="Leave here what you are looking for?"
-                            name="comment"
-                            required
-                        ></textarea>
-                    </div>
-
-                    <button type="button" onClick={() => setAberto(true)}>
-                        SEND
-                    </button>
-                </form>
+                </div>
             </section>
 
             {aberto ? (
                 <section className="contact_alert" id="contact_alert">
                     <div className="contact_box">
-                        <h1 className="text-2xl">
+                        <h1 className="text_2xl">
                             Your message was sent successfully
                         </h1>
-                        <p className="mb-2 mt-4">Click "here" to go back</p>
+                        <p className="mb_2 mt_4">Click "here" to go back</p>
                         <button
-                            className="mt-4"
+                            className="mt_4"
                             onClick={() => setAberto(false)}
                         >
                             Here
